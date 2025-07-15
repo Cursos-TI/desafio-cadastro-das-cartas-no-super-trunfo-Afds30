e98d3dff0ae8206a8f07f4babaa9a1ba086000c1
Adson Sarmento <95049268+Afds30@users.noreply.github.com>
2025-07-15 17:52:22 +0000

    Desafio - nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,15 +13,20 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
+    char nome1[50];
 
     // Declaração das variáveis para a segunda carta
     int populacao2;
     float area2;
     float pib2;
     int pontosTuristicos2;
+    char nome2[50];
 
     // --- Cadastro da Primeira Carta ---
     printf("--- Cadastro da Primeira Carta ---\n");
+
+    printf("Digite o nome da primeira cidade: ");
+    scanf("%s", &nome1);
 
     printf("Digite a populacao da primeira cidade: ");
     scanf("%d", &populacao1);
@@ -40,6 +45,9 @@ int main() {
     // --- Cadastro da Segunda Carta ---
     printf("--- Cadastro da Segunda Carta ---\n");
 
+    printf("Digite o nome da primeira cidade: ");
+    scanf("%s", &nome2);
+
     printf("Digite a populacao da segunda cidade: ");
     scanf("%d", &populacao2);
 
@@ -56,6 +64,7 @@ int main() {
 
     // --- Exibição dos Dados Cadastrados ---
     printf("--- Dados da Primeira Carta ---\n");
+    printf("nome do aluno: %s\n", nome1);
     printf("Populacao: %d\n", populacao1);
     printf("Area: %.2f km2\n", area1);
     printf("PIB: %.2f bilhoes\n", pib1);
@@ -63,6 +72,7 @@ int main() {
     printf("\n");
 
     printf("--- Dados da Segunda Carta ---\n");
+    printf("nome do aluno: %s\n", nome2);
     printf("Populacao: %d\n", populacao2);
     printf("Area: %.2f km2\n", area2);
     printf("PIB: %.2f bilhoes\n", pib2);
